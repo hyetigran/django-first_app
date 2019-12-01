461cc8841fb5f0645145908a5d23b368b9d03b9f
hyetigran <tigran.asriyan@gmail.com>
2019-12-01 19:58:12 +0400

    added templates and connected to settings file

--- first_app/views.py
+++ first_app/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("hello world!")
+    my_dict = {'insert_me': "Hello I'm from views.py"}
+    return render(request, 'first_app/index.html', context=my_dict)
