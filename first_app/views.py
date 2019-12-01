bed90a8293286787002c05f6de28ab952b60fc0c
hyetigran <tigran.asriyan@gmail.com>
2019-12-01 20:37:13 +0400

    create second page with templates

--- first_app/views.py
+++ first_app/views.py
@@ -6,3 +6,8 @@ from django.http import HttpResponse
 def index(request):
     my_dict = {'insert_me': "Hello I'm from views.py"}
     return render(request, 'first_app/index.html', context=my_dict)
+
+
+def help(request):
+    help_dict = {'help_me': 'hello from views.py'}
+    return render(request, 'first_app/help.html', context=help_dict)
